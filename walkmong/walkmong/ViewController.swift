935a4d9e4666f01ed7a87be471b45e1f58c1e4a6
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-02 17:34:01 +0900

    chore: initial project setup

---
+++ walkmong/walkmong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  walkmong
+//
+//  Created by 신호연 on 11/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
