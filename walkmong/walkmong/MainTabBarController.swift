9877d69ca8d7c6dddba2b861485ffd436aeadafa
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-03 22:26:28 +0900

    feat: 메인 탭바 아이콘 svg 추가

--- walkmong/walkmong/MainTabBarController.swift
+++ walkmong/walkmong/MainTabBarController.swift
@@ -1,11 +1,5 @@
-//
-//  MainTabBarController.swift
-//  walkmong
-//
-//  Created by 황채웅 on 11/2/24.
-//
-
 import UIKit
+
 final class MainTabBarController: UITabBarController {
     
     override func viewDidLoad() {
@@ -14,9 +8,24 @@ final class MainTabBarController: UITabBarController {
         setUI()
     }
     
-    private func configureTabBars() -> [UIViewController]{
-        //TODO: 뷰와 탭바 아이템 이미지 추가
-        return []
+    private func configureTabBars() -> [UIViewController] {
+        let matchingVC = MatchingViewController()
+        matchingVC.view.backgroundColor = .white
+        matchingVC.tabBarItem = UITabBarItem(title: "산책 구하기", image: UIImage(named: "MatchingApplyProcedureIcon"), tag: 0)
+        
+        let matchingStateVC = UIViewController()
+        matchingStateVC.view.backgroundColor = .white
+        matchingStateVC.tabBarItem = UITabBarItem(title: "매칭 현황", image: UIImage(named: "MatchingStateIcon"), tag: 1)
+        
+        let talkVC = UIViewController()
+        talkVC.view.backgroundColor = .white
+        talkVC.tabBarItem = UITabBarItem(title: "워크멍톡", image: UIImage(named: "WalkMongTalkIcon"), tag: 2)
+        
+        let mypageVC = UIViewController()
+        mypageVC.view.backgroundColor = .white
+        mypageVC.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(named: "MypageIcon"), tag: 3)
+        
+        return [matchingVC, matchingStateVC, talkVC, mypageVC]
     }
     
     private func setUI() {
