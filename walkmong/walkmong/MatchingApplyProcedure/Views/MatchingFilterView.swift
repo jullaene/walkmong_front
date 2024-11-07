ec752fb0c0f1a7771e77b39088bf31a4e098356c
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-07 12:08:07 +0900

    feat: MatchingFilterView 각 요소 위치 잡기
    - 탭바 가릴 필요 있음

--- walkmong/walkmong/MatchingApplyProcedure/Views/MatchingFilterView.swift
+++ walkmong/walkmong/MatchingApplyProcedure/Views/MatchingFilterView.swift
@@ -6,22 +6,72 @@
 //
 
 import UIKit
+import SnapKit
 
 class MatchingFilterView: UIView {
 
+    private let distanceFrame = UIView()
+    private let breedFrame = UIView()
+    private let matchingStatusFrame = UIView()
+    private let buttonFrame = UIView()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         setupView()
+        setupLayout()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func setupView() {
         self.backgroundColor = .white
         self.layer.cornerRadius = 30
         self.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+
+        // 각각의 프레임 배경색 설정 (위치 확인용)
+        distanceFrame.backgroundColor = .red
+        breedFrame.backgroundColor = .green
+        matchingStatusFrame.backgroundColor = .blue
+        buttonFrame.backgroundColor = .yellow
+
+        // 프레임 추가
+        self.addSubview(distanceFrame)
+        self.addSubview(breedFrame)
+        self.addSubview(matchingStatusFrame)
+        self.addSubview(buttonFrame)
+    }
+
+    private func setupLayout() {
+        // 거리 프레임
+        distanceFrame.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(25)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(145)
+        }
+
+        // 견종 프레임
+        breedFrame.snp.makeConstraints { make in
+            make.top.equalTo(distanceFrame.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(156)
+        }
+
+        // 매칭 여부 프레임
+        matchingStatusFrame.snp.makeConstraints { make in
+            make.top.equalTo(breedFrame.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(114)
+        }
+
+        // 버튼 프레임 (탭바 덮기)
+        buttonFrame.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            // superview의 bottom에 맞추어 배치
+            make.bottom.equalToSuperview().inset(38)
+            make.width.equalTo(356)
+            make.height.equalTo(54)
+        }
     }
 }
