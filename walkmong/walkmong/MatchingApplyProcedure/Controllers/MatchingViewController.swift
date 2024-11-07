f10d8a2c0370eee44e34f314e1d5f090e91f9134
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-08 08:02:51 +0900

    fix: 필터 뷰가 하단 탭뷰에 가리는 문제 해결

--- walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingViewController.swift
+++ walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingViewController.swift
@@ -8,7 +8,7 @@ class MatchingViewController: UIViewController {
     private var dropdownView: DropdownView!
     private var locationSelectView: UIView!
     private var locationLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,10 +35,12 @@ class MatchingViewController: UIViewController {
     }
     
     private func setupDimView() {
+        let parentView = self.tabBarController?.view ?? self.view // TabBarController가 없으면 self.view 사용
+
         dimView = UIView()
         dimView.backgroundColor = UIColor(white: 0, alpha: 0.5)
         dimView.isHidden = true
-        self.view.addSubview(dimView)
+        parentView?.addSubview(dimView) // TabBarController의 view 또는 self.view에 추가
         
         dimView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
@@ -84,21 +86,23 @@ class MatchingViewController: UIViewController {
 
     private func showMatchingFilterView() {
         guard matchingFilterView == nil else { return }
+        
+        let parentView = self.tabBarController?.view ?? self.view // TabBarController가 없으면 self.view 사용
 
         let filterView = MatchingFilterView()
         filterView.layer.cornerRadius = 30
         filterView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         
-        self.view.addSubview(filterView)
+        parentView?.addSubview(filterView) // TabBarController의 view 또는 self.view에 추가
         self.matchingFilterView = filterView
         
         filterView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(605)
-            make.bottom.equalToSuperview().offset(605)
+            make.bottom.equalToSuperview().offset(605) // TabBar와 연결, TabBar가 없어도 동일하게 동작
         }
         
-        self.view.layoutIfNeeded()
+        parentView?.layoutIfNeeded()
 
         dimView.isHidden = false
         
@@ -106,18 +110,19 @@ class MatchingViewController: UIViewController {
             filterView.snp.updateConstraints { make in
                 make.bottom.equalToSuperview()
             }
-            self.view.layoutIfNeeded()
+            parentView?.layoutIfNeeded()
         })
     }
     
     private func hideMatchingFilterView() {
         guard let filterView = matchingFilterView else { return }
+        guard let tabBarView = self.tabBarController?.view else { return }
         
         UIView.animate(withDuration: 0.4, delay: 0, options: [.curveEaseIn], animations: {
             filterView.snp.updateConstraints { make in
-                make.bottom.equalToSuperview().offset(605)
+                make.bottom.equalTo(tabBarView.snp.bottom).offset(605)
             }
-            self.view.layoutIfNeeded()
+            tabBarView.layoutIfNeeded()
         }, completion: { _ in
             filterView.removeFromSuperview()
             self.matchingFilterView = nil
