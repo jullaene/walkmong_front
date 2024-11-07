3869b31b068be4248af6f8824f246eff9944901b
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-08 08:20:36 +0900

    fix: DropdownView가 dimView에 가려지지 않도록 수정
    - MatchingViewController 내 dropdownView를 MainTabBarController의 뷰에 직접 추가하여 계층 문제 해결
    - dropdownView와 dimView의 계층 구조를 조정하여 dropdownView가 항상 dimView 위에 위치하도록 설정
    - showDropdownView 및 hideDropdownView 메서드에서 dropdownView의 위치와 visibility 조정
    - setupDimView 및 setupDropdownView 메서드 추가로 MainTabBarController에서 dimView와 dropdownView 초기화
    - MatchingFilterView 관련 레이아웃 조정

--- walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingViewController.swift
+++ walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingViewController.swift
@@ -4,10 +4,15 @@ import SnapKit
 class MatchingViewController: UIViewController {
 
     private var matchingFilterView: MatchingFilterView?
-    private var dimView: UIView!
-    private var dropdownView: DropdownView!
+    private var dropdownView: DropdownView! {
+        return (self.tabBarController as? MainTabBarController)?.dropdownView
+    }
     private var locationSelectView: UIView!
     private var locationLabel: UILabel!
+    
+    var dimView: UIView? {
+        return (self.tabBarController as? MainTabBarController)?.dimView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,39 +34,7 @@ class MatchingViewController: UIViewController {
         self.locationSelectView = matchingView.locationSelectView
         self.locationLabel = matchingView.locationLabel
         
-        setupDimView()
-        setupDropdownView()
         setupLocationSelectGesture()
-    }
-    
-    private func setupDimView() {
-        let parentView = self.tabBarController?.view ?? self.view // TabBarController가 없으면 self.view 사용
-
-        dimView = UIView()
-        dimView.backgroundColor = UIColor(white: 0, alpha: 0.5)
-        dimView.isHidden = true
-        parentView?.addSubview(dimView) // TabBarController의 view 또는 self.view에 추가
-        
-        dimView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideFilterAndDropdown))
-        dimView.addGestureRecognizer(tapGesture)
-    }
-    
-    private func setupDropdownView() {
-        dropdownView = DropdownView()
-        dropdownView.isHidden = true
-        dropdownView.delegate = self
-        self.view.addSubview(dropdownView)
-        
-        dropdownView.snp.makeConstraints { make in
-            make.width.equalTo(201)
-            make.height.equalTo(130)
-            make.leading.equalTo(locationSelectView.snp.leading)
-            make.top.equalTo(locationSelectView.snp.bottom).offset(10)
-        }
     }
     
     private func setupLocationSelectGesture() {
@@ -70,16 +43,28 @@ class MatchingViewController: UIViewController {
     }
     
     @objc private func showDropdownView() {
-        dimView.isHidden = false
+        guard let dropdownView = dropdownView else { return }
+        
+        dimView?.isHidden = false
         dropdownView.isHidden = false
+
+        dimView?.superview?.bringSubviewToFront(dimView!)
+        dimView?.superview?.bringSubviewToFront(dropdownView)
+        
+        dropdownView.snp.remakeConstraints { make in
+            make.width.equalTo(201)
+            make.height.equalTo(130)
+            make.leading.equalTo(locationSelectView.snp.leading)
+            make.top.equalTo(locationSelectView.snp.bottom).offset(10)
+        }
     }
     
     @objc private func hideDropdownView() {
-        dropdownView.isHidden = true
-        dimView.isHidden = matchingFilterView == nil // 필터 뷰가 없으면 dimView 숨김
+        dropdownView?.isHidden = true
+        dimView?.isHidden = matchingFilterView == nil // 다른 요소가 없으면 dimView 숨김
     }
-    
-    @objc private func hideFilterAndDropdown() {
+
+    @objc func hideFilterAndDropdown() {
         hideDropdownView()
         hideMatchingFilterView()
     }
@@ -87,24 +72,26 @@ class MatchingViewController: UIViewController {
     private func showMatchingFilterView() {
         guard matchingFilterView == nil else { return }
         
-        let parentView = self.tabBarController?.view ?? self.view // TabBarController가 없으면 self.view 사용
+        let parentView = self.tabBarController?.view ?? self.view
 
         let filterView = MatchingFilterView()
         filterView.layer.cornerRadius = 30
         filterView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         
-        parentView?.addSubview(filterView) // TabBarController의 view 또는 self.view에 추가
+        parentView?.addSubview(filterView)
         self.matchingFilterView = filterView
         
         filterView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(605)
-            make.bottom.equalToSuperview().offset(605) // TabBar와 연결, TabBar가 없어도 동일하게 동작
+            make.bottom.equalToSuperview().offset(605)
         }
         
         parentView?.layoutIfNeeded()
 
-        dimView.isHidden = false
+        dimView?.isHidden = false
+        dimView?.superview?.bringSubviewToFront(dimView!) // dimView를 최상단으로 위치시키기
+        parentView?.bringSubviewToFront(filterView)       // matchingFilterView를 dimView 위로 가져오기
         
         UIView.animate(withDuration: 0.4, delay: 0, options: [.curveEaseOut], animations: {
             filterView.snp.updateConstraints { make in
@@ -126,16 +113,16 @@ class MatchingViewController: UIViewController {
         }, completion: { _ in
             filterView.removeFromSuperview()
             self.matchingFilterView = nil
-            self.dimView.isHidden = true
+            self.dimView?.isHidden = true
         })
     }
 }
 
-// DropdownViewDelegate를 통해 선택된 동네를 업데이트합니다.
+// DropdownViewDelegate를 통해 선택된 동네 업데이트
 extension MatchingViewController: DropdownViewDelegate {
     func didSelectLocation(_ location: String) {
         locationLabel.text = location
-        dropdownView.updateSelection(selectedLocation: location)
+        dropdownView?.updateSelection(selectedLocation: location)
         hideDropdownView()
     }
 }
