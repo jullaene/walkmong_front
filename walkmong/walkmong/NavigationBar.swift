ba19965bf4e34d31e68a8713982e6457e3bcc325
woongaaaa <woonge0930@naver.com>
2024-11-08 18:50:15 +0900

    feat: ProgressBar 추가

--- walkmong/walkmong/NavigationBar.swift
+++ walkmong/walkmong/NavigationBar.swift
@@ -13,7 +13,7 @@ extension UIViewController {
     func addCustomNavigationBar(titleText: String = "", showLeftBarButton: Bool, showCloseBarButton: Bool, showRefreshBarButton: Bool) {
         
         let navigationBarView = UIView()
-        navigationBarView.backgroundColor = .gray100
+        navigationBarView.backgroundColor = .white
         self.view.addSubview(navigationBarView)
         self.navigationItem.titleView = navigationBarView
         
@@ -26,7 +26,7 @@ extension UIViewController {
         let titleLabel: UILabel = {
             let label = UILabel()
             label.text = titleText
-            label.font = .systemFont(ofSize: 20, weight: .semibold)
+            label.font = UIFont(name: "Pretendard-SemiBold", size: 20)
             label.textColor = .mainBlack
             label.textAlignment = .center
             return label
@@ -80,6 +80,47 @@ extension UIViewController {
                 make.height.width.equalTo(40)
                 make.trailing.equalToSuperview().offset(-20)
             }
+        }
+    }
+    
+    func addProgressBar(currentStep:Int, totalSteps:Int){
+        let backgroundView: UIView = {
+            let view = UIView()
+            view.backgroundColor = .white
+            return view
+        }()
+        let progressBackgroundView: UIView = {
+            let view = UIView()
+            view.backgroundColor = .gray200
+            view.layer.cornerRadius = 2
+            return view
+        }()
+        let progressView: UIView = {
+            let view = UIView()
+            view.backgroundColor = .mainBlue
+            view.layer.cornerRadius = 2
+            return view
+        }()
+        
+        self.view.addSubview(backgroundView)
+        backgroundView.addSubview(progressBackgroundView)
+        backgroundView.addSubview(progressView)
+        
+        backgroundView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview()
+            make.top.equalToSuperview().offset(119)
+            make.height.equalTo(35)
+        }
+        progressBackgroundView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.top.equalToSuperview().offset(7)
+            make.height.equalTo(4)
+        }
+        progressView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(20)
+            make.top.equalToSuperview().offset(7)
+            make.width.equalTo((view.bounds.width-40)*CGFloat(currentStep)/CGFloat(totalSteps))
+            make.height.equalTo(4)
         }
     }
 }
